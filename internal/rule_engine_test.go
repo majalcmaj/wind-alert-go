4ead76174206b37b0dbaffef3417b235a9ede1e3
Michał Ciesielski <majalcmaj@gmail.com>
2025-10-14 00:29:48 +0200

    Improve the test suite

--- internal/rule_engine_test.go
+++ internal/rule_engine_test.go
@@ -19,12 +19,20 @@ func TestPassingEmptyRule(t *testing.T) {
 }
 
 
-func TestPassingNotMatchingWindSpeed(t *testing.T) {
+func TestPassingNoMatchingRules(t *testing.T) {
 	reading := WeatherReading{WindSpeed: 8.0, WindAngle: 20.1}
 	rules := []Rule{
-		{
+		{ // Not matching anything
 			SpeedRange: Range{From: 0.0, To: 7.0},
+			AngleRange: Range{From: 95.0, To: 130.37},
+		},
+		{ // Not matching speed
+			SpeedRange: Range{From: 20.1, To: 30.4},
 			AngleRange: Range{From: 15.0, To: 21.37},
+		},
+		{ // Not matching angle
+			SpeedRange: Range{From: 7.0, To: 13.5},
+			AngleRange: Range{From: 0.2, To: 2.137},
 		},
 	}
 
@@ -34,35 +42,24 @@ func TestPassingNotMatchingWindSpeed(t *testing.T) {
 		t.Errorf("Got an error: %v", err)
 	}
 	if result != false {
-		t.Errorf("Rule engine should return false for a data not matching wind speed range in the passed rule")
-	}
-}
-
-func TestPassingNotMatchingWindAngle(t *testing.T) {
-	reading := WeatherReading{WindSpeed: 8.0, WindAngle: 22.4}
-	rules := []Rule{
-		{
-			SpeedRange: Range{From: 0.0, To: 9.0},
-			AngleRange: Range{From: 15.0, To: 21.37},
-		},
-	}
-
-	result, err := RunRuleEngine(reading, &rules)
-
-	if err != nil {
-		t.Errorf("Got an error: %v", err)
-	}
-	if result != false {
-		t.Errorf("Rule engine should return false for a data not matching wind angle range in the passed rule")
+		t.Errorf("Rule engine should return false for no matching rules")
 	}
 }
 
 func TestPassingSingleMatchingRule(t *testing.T) {
 	reading := WeatherReading{WindSpeed: 8.0, WindAngle: 20.1}
 	rules := []Rule{
-		{
+		{ // Not matching rule
+			SpeedRange: Range{From: 0.0, To: 7.0},
+			AngleRange: Range{From: 95.0, To: 130.37},
+		},
+		{ // Finally - a matching rule
 			SpeedRange: Range{From: 0.0, To: 10.0},
 			AngleRange: Range{From: 15.0, To: 21.37},
+		},
+		{ // Another not matching angle
+			SpeedRange: Range{From: 7.0, To: 13.5},
+			AngleRange: Range{From: 0.2, To: 2.137},
 		},
 	}
 
