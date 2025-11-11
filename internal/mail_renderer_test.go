2bc2a02f26649a7dd7b12e1491b1d2da5160df5b
Michał Ciesielski <majalcmaj@gmail.com>
2025-11-12 00:22:48 +0100

    Red test for the table rendering

--- internal/mail_renderer_test.go
+++ internal/mail_renderer_test.go
@@ -1,8 +1,11 @@
 package internal
 
 import (
+	"fmt"
+	"regexp"
 	"strings"
 	"testing"
+	"time"
 )
 
 func TestRenderingMailDisplaysTitle(t *testing.T) {
@@ -34,5 +37,54 @@ func TestRenderingLatLonInformation(t *testing.T) {
 
 	if !strings.Contains(result, "40.721370,-74.154970") {
 		t.Errorf("Expected lat/lon information to be present but got '%s'", result)
+	}
+}
+
+func TestRenderingDailyAndHourlyTables(t *testing.T) {
+	reading := WeatherReading{
+		Lat:      40.72137,
+		Lon:      -74.15497,
+		Readings: map[string]*[]WindDataPoint{
+			"daily":  {
+				{Time: parseTime("2025-01-01T10:00"), WindSpeed: 10, WindAngle: 180},
+				{Time: parseTime("2025-01-02T10:00"), WindSpeed: 20, WindAngle: 80},
+			},
+			"hourly": {
+				{Time: parseTime("2025-01-01T10:00"), WindSpeed: 10, WindAngle: 180},
+				{Time: parseTime("2025-01-01T11:00"), WindSpeed: 12, WindAngle: 190},
+			},
+		},
+	}
+
+	renderedMail, err := RenderMail(&reading)
+
+	if err != nil {
+		t.Fatalf("Expected no error but got: %v", err)
+	}
+
+	for _, row := range *reading.Readings["daily"] {
+	  matchRow(t, renderedMail, row)
+	}
+
+	for _, row := range *reading.Readings["hourly"] {
+	  matchRow(t, renderedMail, row)
+	}
+}
+
+func parseTime(tStr string) time.Time {
+	tm, err := time.Parse(time.RFC3339, tStr + ":00Z")
+	if err != nil {
+		panic(err)
+	}
+	return tm
+}
+
+func matchRow(t *testing.T, mailHtml string, row WindDataPoint) {
+	tableRegEx := regexp.MustCompile(
+		fmt.Sprintf(`(?s)<tr.*>.*<td.*>%s</td>.*<td.*>%.1f</td>.*<td.*>%.1f</td>.*</tr>`, row.Time.Format("2006-01-02 15:04"), row.WindSpeed, row.WindAngle))
+
+	match := tableRegEx.MatchString(mailHtml)
+	if !match {
+		t.Errorf("Expected WindDataPoint %+v not found in:\n%s", row, mailHtml)
 	}
 }
