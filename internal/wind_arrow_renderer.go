5a93b0ea5268147ce7e88f0a509099281f3029e1
Michał Ciesielski <majalcmaj@gmail.com>
2025-11-29 23:39:03 +0100

    Fix wind arrows display

--- internal/wind_arrow_renderer.go
+++ internal/wind_arrow_renderer.go
@@ -6,21 +6,21 @@ func renderWindArrow(angle float64) string {
 	angle = math.Mod(angle, 360)
 	switch {
 	case angle >= 337.5 || angle < 22.5:
-		return "↑" // North
+		return "↑\uFE0E" // North
 	case angle < 67.5:
-		return "↗" // Northeast
+		return "↗\uFE0E" // Northeast
 	case angle < 112.5:
-		return "→" // East
+		return "→\uFE0E" // East
 	case angle < 157.5:
-		return "↘" // Southeast
+		return "↘\uFE0E" // Southeast
 	case angle < 202.5:
-		return "↓" // South
+		return "↓\uFE0E" // South
 	case angle < 247.5:
-		return "↙" // Southwest
+		return "↙\uFE0E" // Southwest
 	case angle < 292.5:
-		return "←" // West
+		return "←\uFE0E" // West
 	case angle >= 292.5:
-		return "↖" // Northwest
+		return "↖\uFE0E" // Northwest
 	default:
 		return "?" // Should never happen
 	}
